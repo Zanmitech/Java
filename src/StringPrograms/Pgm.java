7b5087fbb6c8fce59784eca026d1ffeb1096078a
Mounika <Mounika@DESKTOP-BSPMN8M>
2018-06-26 12:47:54 +0530

    printing table code

--- src/StringPrograms/Pgm.java
+++ src/StringPrograms/Pgm.java
@@ -1,5 +1,28 @@
 package StringPrograms;
 
 public class Pgm {
+		
+		public static void table(int r,int c) {
+			System.out.println("<table>"); 
+			for(int i=0;i<r;i++) {
+				System.out.println("<tr>");
+				  for(int j=0;j<c;j++) {
+					  System.out.println("<td> \t </td>");
+				  }
+				  System.out.println("</tr>");
+			}
+			
+				  System.out.println("</table>");
+			}
+		
 
-}
+		public static void main(String[] args) {
+		
+			table(2, 2);
+	         
+			
+		}
+
+	}
+
+
