bcc4918f02396adecd6f6746ae11c7ded114a000
sai <sai@LAPTOP-AN0OGE7M>
2018-07-20 23:52:17 +0530

    Reverse String

--- src/sravanthi/ReverseString.java
+++ src/sravanthi/ReverseString.java
@@ -8,9 +8,9 @@ public class ReverseString {
 		String original,reverse="";
 		Scanner s=new Scanner(System.in);
 		System.out.println("enter a string:");
-	original=s.nextLine();
-	int length=original.length();
-	for(int i=length-1;i>=0;i--) {
+		original=s.nextLine();
+		int length=original.length();
+		for(int i=length-1;i>=0;i--) {
 		reverse=reverse+original.charAt(i);
 	}
 		System.out.println("Reverse of entered string:"+reverse);
