86196ae72edb1b22aa548eddeb145615f16664ce
sai <sai@LAPTOP-AN0OGE7M>
2018-07-27 23:07:12 +0530

    array program

--- src/sravanthi/DuplicantStrings.java
+++ src/sravanthi/DuplicantStrings.java
@@ -3,7 +3,7 @@ package sravanthi;
 public class DuplicantStrings {
 
 	public static void main(String[] args) {
-		String arr[]= {"java","html","sql","java","sql","selenium","manual","phython"};
+		String arr[]= {"java","html","sql","java","sql","selenium","manual","phython","javascript"};
 		for(int i=0;i<=arr.length-1;i++) {
 			for(int j=i+1;j<arr.length;j++) {
 				if(arr[i].equals(arr[j])&&(i!=j)) {
--- src/sravanthi/RemoveSpecificElementInArray.java
+++ src/sravanthi/RemoveSpecificElementInArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RemoveSpecificElementInArray {
 
 	public static void main(String[] args) {
-		int[]arr= {25,40,56,32,89,76,45,27};
+		int[]arr= {25,40,56,32,89,76,45,27,61};
 		   System.out.println("Original Array : "+ Arrays.toString(arr)); 
 		   for(int i=1;i<arr.length-1;i++) {
 			arr[i]=arr[i]+1;
