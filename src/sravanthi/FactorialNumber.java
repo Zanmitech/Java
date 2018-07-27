9c34544087f469d4658092132518278b1101a901
sai <sai@LAPTOP-AN0OGE7M>
2018-07-27 23:16:33 +0530

    star and factorial program

---
+++ src/sravanthi/FactorialNumber.java
@@ -0,0 +1,16 @@
+package sravanthi;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		int num=6;
+		int fact=1;
+		for(int i=1;i<=num;i++) {
+			fact=fact*i;
+		}
+		System.out.println("factorial "+num +" is:"+fact);
+	}
+
+	}
+
+
